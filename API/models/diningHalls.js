4436007446c780dea6f3e2d2059abd76c8a5f9cc
Jancarlos Sosa <sosaj2021@gmail.com>
2025-01-12 10:10:50 -0500

    update hall name variable

--- API/models/diningHalls.js
+++ API/models/diningHalls.js
@@ -11,7 +11,7 @@ DiningHalls.init(
             primaryKey: true,
             autoIncrement: true,
         },
-        name: {
+        hallname: {
             type: DataTypes.STRING,
             allowNull: false,
         },
