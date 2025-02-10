a360195c1a57de1705aed7c140a91798adafc971
Jancarlos Sosa <sosaj2021@gmail.com>
2025-02-10 16:36:28 -0500

    add dining stations model

--- API/models/init.js
+++ API/models/init.js
@@ -1,10 +1,13 @@
 import DiningHalls from "./diningHalls.js";
 import DiningHours from "./diningHours.js";
+import DiningStations from "./diningStations.js";
 import ScheduleItems from "./scheduleItems.js";
 
 
 // Set up associations
-DiningHalls.associate({ DiningHours });
+DiningHalls.associate({ DiningHours, DiningStations });
 DiningHours.associate({ DiningHalls });
+DiningStations.associate({ DiningHalls });
+
 
 export { DiningHalls, DiningHours, ScheduleItems };
