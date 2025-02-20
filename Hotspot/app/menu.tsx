ef1b1ebffdb0c9b5945430e0e93e76f17de187d6
Jancarlos Sosa <60488065+Sosa-IQ@users.noreply.github.com>
2025-02-20 12:35:41 -0500

    update schedule interface

--- Hotspot/app/menu.tsx
+++ Hotspot/app/menu.tsx
@@ -7,66 +7,83 @@ import { format } from 'date-fns';
 
 // Interface for dining hall data
 interface Schedule {
-    food: string;
+  scheduleid: number,
+  scheduledate: string,
+  foodid: number,
+  food: string,
+  isbreakfast: boolean,
+  islunch: boolean,
+  isdinner: boolean,
+  allergens: string,
+  summary: null,
+  imagelink: null,
+  diningstationid: number,
+  dininghallid: number,
+  station: {
+    diningstationid: number,
+    dininghallid: number,
+    stationname: string
   }
+}
 
 // Identify date
 const date = new Date();
 const formattedDate: string = format(date, 'yyyy-MM-dd');
 
 const menu = () => {
-    // grab name and id from dining.tsx (from DiningItem component)
-    const { name, id } = useLocalSearchParams() as { name: string; id: string };
-    const navigation = useNavigation();
+  // grab name and id from dining.tsx (from DiningItem component)
+  const { name, id } = useLocalSearchParams() as { name: string; id: string };
+  const navigation = useNavigation();
 
-    useLayoutEffect(() => {
-        if (name) {
-          navigation.setOptions({ title: name }); // Set the screen title to the "name"
-        }
-      }, [name, navigation]);
+  useLayoutEffect(() => {
+    if (name) {
+      navigation.setOptions({ title: name }); // Set the screen title to the "name"
+    }
+  }, [name, navigation]);
 
-      const [schedule, setSchedule] = useState<Schedule[]>([]);
-    
-      // Fetch dining hall data on component mount
-      useEffect(() => {
-        const fetchSchedule = async () => {
-          try {
-            const response = await scheduleService.getSchedule(id, formattedDate);
-            setSchedule(response.schedule);
-          } catch (error) {
-            console.error(error);
-          }
-        };
-    
-        fetchSchedule();
-      }, []);
+  const [schedule, setSchedule] = useState<Schedule[]>([]);
+  
+  // Fetch dining hall data on component mount
+  useEffect(() => {
+    const fetchSchedule = async () => {
+      try {
+        const response = await scheduleService.getSchedule(id, formattedDate);
+        setSchedule(response.schedule);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-    return (
-        <ScrollView style={styles.container}>
-            <Text style={styles.title}>{formattedDate}</Text>
-            {schedule.map((item, index) => (
-            <Text key={index} style={styles.item}>
-                - {item.food}
-            </Text>
-            ))}
-      </ScrollView>
-    );
+    fetchSchedule();
+  }, []);
+  
+
+  return (
+    <ScrollView style={styles.container}>
+      <Text style={styles.title}>{formattedDate}</Text>
+      {schedule.map((item, index) => (
+      <Text key={index} style={styles.item}>
+        - {item.food}
+      </Text>
+      ))}
+    </ScrollView>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 16,
-    },
-    title: {
-      fontSize: 24,
-      fontWeight: 'bold',
-      marginBottom: 16,
-    },
-    item: {
-      fontSize: 18,
-      marginBottom: 8,
-    },
-  });
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  item: {
+    fontSize: 18,
+    marginBottom: 8,
+  },
+});
 
 export default menu
