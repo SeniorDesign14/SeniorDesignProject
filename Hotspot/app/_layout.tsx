114ff702351904e23bc04aa7d6c319e6e349aeff
miw19008 <miw19008@soc-sdp-14.grove.ad.uconn.edu>
2025-02-23 13:15:26 -0500

    hopefully works

--- Hotspot/app/_layout.tsx
+++ Hotspot/app/_layout.tsx
@@ -1,9 +1,11 @@
 import { Stack } from "expo-router";
+import { ScreenStackHeaderBackButtonImage } from "react-native-screens";
 
 export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" />
+      <Stack.Screen name="login" options={{ headerShown: false }} />
       <Stack.Screen name="(tabs)" options={{ headerShown: false, gestureEnabled: false }} />
       <Stack.Screen name="menu" options={{ headerShown: true, title: "Menu" }} />
     </Stack>
