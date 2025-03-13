1ed9665c704afcf5da68c21442dd4c5f203e67e2
Mitchell Worthington <mitchellworthington64@gmail.com>
2025-03-13 13:12:49 -0400

    made new branch as other is corrupt

---
+++ API/models/menu.js
@@ -0,0 +1,28 @@
+import { Model, DataTypes } from 'sequelize';
+import sequelize from '../database.js'; // Import the connection to the database
+
+class Menu extends Model {}
+
+Menu.init(
+    {
+        foodID: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        food: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        modelName: 'menu',
+        timestamps: false,
+    }
+);
+
+// define associations
+
+export default Menu;
--- Hotspot/app/index.tsx
+++ Hotspot/app/index.tsx
@@ -1,8 +1,15 @@
 import { Text, View } from "react-native";
 import { useRouter, Redirect } from 'expo-router';
 
+// direct to tabs list when bootup
+// export default function Index() {
+//   return (
+//     <Redirect href="/(tabs)/dining" />
+//   );
+// }
+ // direct to login screen when bootup
 export default function Index() {
   return (
-    <Redirect href="/(tabs)/dining" />
+    <Redirect href="/login" />
   );
 }
