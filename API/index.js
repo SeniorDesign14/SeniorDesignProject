00494e40a1e19a367d770bcf5bc8289dfa3c2fd7
Jancarlos Sosa <sosaj2021@gmail.com>
2025-03-23 19:55:32 -0400

    add menu route in index

--- API/index.js
+++ API/index.js
@@ -3,6 +3,7 @@ import dininghallsRoutes from './routes/dininghalls.js';
 import scheduleRoutes from './routes/schedule.js';
 import userRoutes from './routes/user.js';
 import favoriteRoutes from './routes/favorite.js';
+import menuRoutes from './routes/menu.js';
 import cors from 'cors';
 const PORT = process.env.PORT || 3000;
 
@@ -21,6 +22,8 @@ app.use('/schedule', scheduleRoutes);
 app.use('/user', userRoutes);
 
 app.use('/favorite', favoriteRoutes);
+
+app.use('/menu', menuRoutes);
 
 app.get('/status', (req, res) => {
     res.status(200).send({'body': 'The server is running!'});
