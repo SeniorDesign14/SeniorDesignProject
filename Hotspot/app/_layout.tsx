a2ca74556648dc6b82af6399940d17218404c352
Jancarlos Sosa <60488065+Sosa-IQ@users.noreply.github.com>
2025-03-23 20:45:57 -0400

    update layout for index

--- Hotspot/app/_layout.tsx
+++ Hotspot/app/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen name="index" options={{ headerShown: false}} />
       <Stack.Screen name="(tabs)" options={{ headerShown: false, gestureEnabled: false }} />
       <Stack.Screen name="menu" options={{ headerShown: true, title: "Menu" }} />
       <Stack.Screen name="foodQuery" options={{ headerShown: false, title: "Food Query" }} />
