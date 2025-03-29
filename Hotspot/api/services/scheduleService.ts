da937367967ae1e09dcd7d14402e3c5886eb2dae
Jancarlos Sosa <60488065+Sosa-IQ@users.noreply.github.com>
2025-03-29 14:50:30 -0400

    add new endpoint

--- Hotspot/api/services/scheduleService.ts
+++ Hotspot/api/services/scheduleService.ts
@@ -3,4 +3,5 @@ import { ENDPOINTS } from "../endpoints";
 
 export const scheduleService = {
     getSchedule: (hallid: string, date: string) => apiClient.get(ENDPOINTS.SCHEDULE + `/${hallid}/${date}`),
+    getScheduleRange: (startDate: string, endDate: string) => apiClient.get(ENDPOINTS.SCHEDULE + `/range/${startDate}/${endDate}`),
 };
