71dbb908c224675b7b6022e5f7f252d2950de814
Jancarlos Sosa <60488065+Sosa-IQ@users.noreply.github.com>
2025-04-10 15:40:46 -0400

    hide header for menu

--- Hotspot/app/_layout.tsx
+++ Hotspot/app/_layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout() {
       <Stack.Screen name="index" options={{ headerShown: false}} />
       {/* <Stack.Screen name="login" options={{ headerShown: false }} /> */}
       <Stack.Screen name="(tabs)" options={{ headerShown: false, gestureEnabled: false }} />
-      <Stack.Screen name="menu" options={{ headerShown: true, title: "Menu" }} />
+      <Stack.Screen name="menu" options={{ headerShown: false, title: "Menu" }} />
       <Stack.Screen name="foodQuery" options={{ headerShown: false, title: "Food Query" }} />
       <Stack.Screen name="favorited" options={{ headerShown: false, title: "Favorited" }} />
       <Stack.Screen name="nutritional" options={{ headerShown: false, title: "Nutritional" }} />
