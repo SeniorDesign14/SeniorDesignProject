f6c69b42e7da5783237a34119fb4a57ade21a6bc
Jancarlos Sosa <60488065+Sosa-IQ@users.noreply.github.com>
2025-04-10 14:44:33 -0400

    add imageUrl

--- Hotspot/types/index.d.ts
+++ Hotspot/types/index.d.ts
@@ -69,7 +69,7 @@ declare interface Schedule {
     },
     time: string,
     isFavorited: boolean
-    imageUri: string;
+    imageUrl: string;
 }
 
 declare interface FoodItem {
@@ -77,5 +77,5 @@ declare interface FoodItem {
     food: string;
     isFavorited: boolean;
     allergens: string;
-    imageUri: string;
+    imageUrl: string;
 }
